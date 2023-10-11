71760356030653a6bc268c32845311f70f314c5c
Hải Nguyễn Duy <nguyenduyhai0018@gmail.com>
2023-10-11 14:36:36 +0700

    update

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://localhost:8080',
+    baseURL:'https://moviereviewbackend-production.up.railway.app/',
     headers: {"ngrok-skip-browser-warning": "true"}
 });
